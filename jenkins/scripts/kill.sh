55ab5f586d52b61f28a031b1cfc56beca3a82cc0
Junius <juniusonline@gmail.com>
2023-07-22 06:38:18 +0700

    Jeda eksekusi pipeline 1 menit

--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -4,4 +4,6 @@ echo 'The following command terminates the "npm start" process using its PID'
 echo '(written to ".pidfile"), all of which were conducted when "deliver.sh"'
 echo 'was executed.'
 set -x
+sleep 60
+echo 'Waktu jeda eksekusi pipeline, mohon tunggu 60 detik..'
 kill $(cat .pidfile)
