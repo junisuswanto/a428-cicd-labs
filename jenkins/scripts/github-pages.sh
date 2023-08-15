a675a541b07797f4108e8c62dee1fca1fa6edcaa
Junius <juniusonline@gmail.com>
2023-08-15 23:00:46 +0700

    Deploy ke github-pages2 fix

--- jenkins/scripts/github-pages.sh
+++ jenkins/scripts/github-pages.sh
@@ -6,6 +6,6 @@ ls build
 git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"
 git config --global user.name "github-actions[bot]"
 # set the target repo
-git remote set-url origin https://git:${GITHUB_TOKEN}@github.com/junisuswanto/a428-cicd-labs.git
+git remote set-url origin https://git:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
 # run gh-pages, please ensure the jenkins have the permission
 npx gh-pages --message '[skip ci] Updates' --dist build
